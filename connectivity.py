676b18afbb0f2ab236a0984d0c7ccc8bc10909d5
weizirui-hi <weizirui2@qq.com>
2025-01-17 16:00:43 +0800

    git测试

---
+++ connectivity.py
@@ -0,0 +1,5 @@
+import requests
+answer = "0"
+def home():
+    print("[1]ping联通性测试\n[2]ping联通性测试（持续）")
+    answer = input("请选择需要运行的项目，并输入其的编号：")
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os
 import sys
-answer = 0
+import connectivity
+answer = "0"
 def cm():
         input("\n按下任意键返回首页")
         home()
 def home():
-    global answer
     os.system("cls")
     print("欢迎使用InternetToolBox！\n[1]连通性测试\n[2]...\n[3]关于\n[0]退出")
-    answer = int(input("请选择需要运行的项目，并输入其的编号："))
+    answer = input("请选择需要运行的项目，并输入其的编号：")
     os.system("cls")
-    if answer == 1:
-        print("连通性测试")
+    if answer == "1":
+        connectivity.home()
         cm()
-    elif answer == 3:
+    elif answer == "3":
         print("关于InternetToolBox")
         print("Copyright © 2025 RMDCXY&CodeGlitch95 All Rights Reserved.")
         print("Github项目地址：https://github.com/Weizirui-hi/InternetToolBox/")
         print("作者空间：\nCodeGlitch95：https://b23.tv/pUIDnHM\nRMDCXY：https://space.bilibili.com/3493142110144946")
         cm()
-    elif answer == 0:
+    elif answer == "0":
           sys.exit()
     else :
         os.system("cls")
